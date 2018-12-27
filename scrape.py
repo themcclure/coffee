2acd3454a3979a89cef971fdb6f88c856b87c9fd
mcclurem <mike.mcclure@soundtransit.org>
2018-12-26 20:59:44 -0800

    initial commit

---
+++ scrape.py
@@ -0,0 +1,10 @@
+"""
+Scrape the roast info from the suppliers website, massage and cache it
+"""
+
+from bs4 import BeautifulSoup
+import requests
+
+page = requests.get('https://www.sweetmarias.com/green-coffee.html')
+c = page.content
+s = BeautifulSoup(c, 'html.parser')
